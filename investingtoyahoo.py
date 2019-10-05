40357946f01c1229a11cc0910b28398f527e4c8a
gulcihancakir <gulcihancakir8@gmail.com>
2019-10-05 23:08:00 +0300

    commit

--- investingtoyahoo.py
+++ investingtoyahoo.py
@@ -18,25 +18,41 @@ with open(filename, newline='') as csvfile:
         
         reverse_list = add_list[::-1]
         Date = "-".join(reverse_list)
-       
         
+        if row[5].endswith('K'):
+            x=row[5].split('K')
+            y=float(x[0].replace(',','.'))
+            row[5]=int(y)*10**3
+
+        elif row[5].endswith('M'):
+            x=row[5].split('M')
+            y=float(x[0].replace(',','.'))
+            row[5]=int(y)*10**6
+
+        elif row[5].endswith('B'):
+            x=row[5].split('B')
+            y=float(x[0].replace(',','.'))
+            row[5]=int(y)*10**9
+
+        elif row[5].endswith('-'):
+            row[5]=0
+       
         d = [Date,
         row[2].replace('.','').replace(',','.'),
         row[3].replace('.','').replace(',','.'),
         row[4].replace('.','').replace(',','.'),
         row[1].replace('.','').replace(',','.'),
-        row[5].replace(',','').replace('K','0'*3).replace('M','0'*6).replace('B','0'*9).replace('-','0')]
-        
-
-        
+        row[5]]
+            
         liste.append(d)
     liste.reverse()
     
-    
-    
-
+    name = filename.split('.')
+    name.insert(1,'-converted.')
+    filename = ''.join(name)
+    print(filename)
     header = ['Date','Open','High','Low','Close','Volume']
-    with open(filename + '-converted','w',newline='') as csvfile:
+    with open(filename,'w',newline='') as csvfile:
             
         writer = csv.writer(csvfile)
         writer.writerow(header)
